bf50b77613ee058cb236c9f5857697e04b2270b5
Irsyaad Budi <irsyaadbp01@gmail.com>
2020-08-29 16:02:42 +0700

    Upload first project

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  /**  Run Migrator */
+  await orm.getMigrator().up();
+
+  /** Create Express Server */
+  const app = express();
+
+  /** Create Apollo Server for Graphql */
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
+
+main().catch((err) => console.log(err));
